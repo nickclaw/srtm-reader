a231533b463fee4a50134e2934ad81a1e8f1435f
Nicholas Clawson <nickclaw@gmail.com>
2014-04-17 21:21:52 -0700

    Pull out global variables, make offset cleaner

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 var fs = require('fs');
 
+var numBytes = 2,
+    matrixSize = 1201;
+
+function getOffset(lat, long) {
+    var rows = Math.floor( Math.abs(lat % 1) * matrixSize ),
+        columns = Math.floor( Math.abs(long % 1) * matrixSize );
+
+    return (rows * matrixSize + columns) * numBytes;
+}
+
 function readHgt(lat, long, callback) {
-    var numBytes = 2,
-        row = Math.floor(Math.abs(lat % 180) * 1201),
-        column = Math.floor(Math.abs(long % 180) * 1201),
-        offset = (row * 1201 + column) * numBytes,
+    var offset = getOffset(lat, long),
         buffer = new Buffer(numBytes);
 
-    fs.open('./files/N47W122.hgt', 'r', function(err, fd) {
+    fs.open('./files/N47W123.hgt', 'r', function(err, fd) {
 
         fs.fstat(fd, function(err, stats) {
             console.log('stats', stats);
@@ -15,10 +22,10 @@ function readHgt(lat, long, callback) {
         });
 
         fs.read(fd, buffer, 0, numBytes, offset, function(err, bytesRead, buffer) {
-            console.log(buffer[1] + ',' + buffer[0], buffer[1] * 256 + buffer[0]);
-            callback && callback(err, buffer[1] * 256 + buffer[0]);
+            console.log(buffer[0] + ',' + buffer[1], buffer[0] * 256 + buffer[1]);
+            callback && callback(err, buffer[0] * 256 + buffer[1]);
         });
     });
 }
 
-readHgt(.5, .5);
+readHgt(47.606209, -122.332070);
