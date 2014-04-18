42140ce7dd595260794162f6cd8143d9879eed48
Nicholas Clawson <nickclaw@gmail.com>
2014-04-17 20:55:02 -0700

    Gets correct offset

--- index.js
+++ index.js
@@ -2,16 +2,21 @@ var fs = require('fs');
 
 function readHgt(lat, long, callback) {
     var numBytes = 2,
-        row = Math.floor(Math.abs(lat % 180) * 3601),
-        column = Math.floor(Math.abs(long % 180) * 3601),
-        offset = (row * 3601 + column) * numBytes,
+        row = Math.floor(Math.abs(lat % 180) * 1201),
+        column = Math.floor(Math.abs(long % 180) * 1201),
+        offset = (row * 1201 + column) * numBytes,
         buffer = new Buffer(numBytes);
 
-    console.log(offset + ' bytes.');
-
     fs.open('./files/N47W122.hgt', 'r', function(err, fd) {
-        fs.read(fd, buffer, 0, numBytes, 10, function(err, bytesRead, buffer) {
-            console.log(buffer[1]* 256 + buffer[0]);
+
+        fs.fstat(fd, function(err, stats) {
+            console.log('stats', stats);
+            console.log('offset', offset);
+        });
+
+        fs.read(fd, buffer, 0, numBytes, offset, function(err, bytesRead, buffer) {
+            console.log(buffer[1] + ',' + buffer[0], buffer[1] * 256 + buffer[0]);
+            callback && callback(err, buffer[1] * 256 + buffer[0]);
         });
     });
 }
