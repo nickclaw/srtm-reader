ce18e7c65d642a846ce484bfdcb4e6c698b3b747
Nicholas <nickclaw@gmail.com>
2014-04-18 17:35:52 -0700

    Fixed service breaking bugs
    Turns out it basically wasn't working at all before...

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var path = require('path'),
 
 var quadrant = new Quadrant(path.join(__dirname, '/files/N47W123.hgt'));
 
-quadrant.read(47.606209, -122.332070, function(err, height) {
+quadrant.read(47.234, -122.2340, function(err, height) {
     console.log(err);
     console.log(height);
 });
