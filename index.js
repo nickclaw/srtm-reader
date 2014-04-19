35f8cd705f4c78506734517a805dbf733cd0a7fa
Nicholas <nickclaw@gmail.com>
2014-04-18 17:56:47 -0700

    Whoops forgot index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+var Quadrant = require('./lib/Quadrant.js'),
+    GraduatedArray = require('./lib/GraduatedArray.js'),
+    GraduatedMatrix = require('./lib/GraduatedMatrix.js');
+
+module.exports = {
+    Quadrant: Quadrant,
+    GraduatedArray: GraduatedArray,
+    GraduatedMatrix: GraduatedMatrix
+};
